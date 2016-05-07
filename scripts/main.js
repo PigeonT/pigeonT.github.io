ba210b36a06a19cb4eda355d9e104438458aef25
pigeonT <pigeon.tian@gmail.com>
2016-05-07 23:33:42 +0200

    dev routing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,41 @@
-console.debug('success!');
+require.config({
+
+	paths: {
+		'angular' : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min', 'libs/angular'],
+		'angularRoute' : ['https://code.angularjs.org/1.5.5/angular-route.min', 'libs/angular-route']
+	},
+
+	shim: {
+		angular: {
+			exports: 'angular'
+		},
+
+		angularRoute : {
+			deps : ['angular'],
+			exports : 'angular-route'
+		}
+	}
+});
+
+require(['angular', 'angularRoute'], function(angular, ngRoute) {
+	'use strict'
+
+	angular.module('pigeonT', ['ngRoute'])
+		.config(['$routeProvider', function($routeProvider) {
+			$routeProvider
+				.when('templates/aboutme.html', {
+					templateUrl: 'templates/aboutme.html',
+					controller: 'aboutmeController' 
+				})
+				.when('/interests', {
+					templateUrl: 'templates/interests.html',
+					controller: 'interestsController' 
+				});
+		}])
+		.controller('containerController', function($scope) {
+			
+		})
+		.controller('aboutmeController', function($scope) {
+        });;
+
+});
