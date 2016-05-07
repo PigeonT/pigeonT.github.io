b9161357b0701d0e27f5099638c1d37dfc93a5ee
pigeonT <pigeon.tian@gmail.com>
2016-05-08 00:38:23 +0200

    routing of angularjs

--- scripts/main.js
+++ scripts/main.js
@@ -30,12 +30,24 @@ require(['angular', 'angularRoute'], function(angular, ngRoute) {
 				.when('/interests', {
 					templateUrl: 'templates/interests.html',
 					controller: 'interestsController' 
+				})
+				.when('/reading', {
+					templateUrl: 'templates/reading.html',
+					controller: 'readingController' 
+				})
+				.when('/contacts', {
+					templateUrl: 'templates/contacts.html',
+					controller: 'contactsController' 
 				});
 		}])
 		.controller('containerController', function($scope) {
 			
 		})
-		.controller('aboutmeController', function($scope) {
-        });;
+		.controller('interestsController', function($scope) {
+        })
+        .controller('readingController', function($scope) {
+        })
+        .controller('contactsController', function($scope) {
+        });
 
 });
