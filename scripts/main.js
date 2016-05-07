eef876913f6b3cc1d418c58e9a425f101254a9e6
pigeonT <pigeon.tian@gmail.com>
2016-05-08 01:06:57 +0200

    require js

--- scripts/main.js
+++ scripts/main.js
@@ -17,29 +17,10 @@ require.config({
 	}
 });
 
-require(['angular', 'angularRoute'], function(angular, ngRoute) {
+require(['route'], function(angular, ngRoute) {
 	'use strict'
 
 	angular.module('pigeonT', ['ngRoute'])
-		.config(['$routeProvider', function($routeProvider) {
-			$routeProvider
-				.when('/aboutme', {
-					templateUrl: 'templates/aboutme.html',
-					controller: 'aboutmeController' 
-				})
-				.when('/interests', {
-					templateUrl: 'templates/interests.html',
-					controller: 'interestsController' 
-				})
-				.when('/reading', {
-					templateUrl: 'templates/reading.html',
-					controller: 'readingController' 
-				})
-				.when('/contacts', {
-					templateUrl: 'templates/contacts.html',
-					controller: 'contactsController' 
-				});
-		}])
 		.controller('containerController', function($scope) {
 			
 		})
--- scripts/route.js
+++ scripts/route.js
@@ -1,3 +1,26 @@
-define(['angularRoute'], function(ngRoute) {
+define(['angular', 'angularRoute'], function(angular, ngRoute) {
+
+	'use strict'
+
+	angular.module('pigeonT', ['ngRoute'])
+		.config(['$routeProvider', function($routeProvider) {
+			$routeProvider
+				.when('/aboutme', {
+					templateUrl: 'templates/aboutme.html',
+					controller: 'aboutmeController' 
+				})
+				.when('/interests', {
+					templateUrl: 'templates/interests.html',
+					controller: 'interestsController' 
+				})
+				.when('/reading', {
+					templateUrl: 'templates/reading.html',
+					controller: 'readingController' 
+				})
+				.when('/contacts', {
+					templateUrl: 'templates/contacts.html',
+					controller: 'contactsController' 
+				});
+		}]);
 
 });
