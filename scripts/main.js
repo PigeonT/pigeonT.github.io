20b3f3249fb065769b49a00373fd98514a5acb00
pigeonT <pigeon.tian@gmail.com>
2016-05-09 01:26:21 +0200

    manuell bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -17,33 +17,46 @@ require.config({
 	}
 });
 
-require(['route'], function(route) {
+require(['angular', 'angularRoute'], function(angular, ngRoute) {
 	'use strict'
 
-	route.config();
+	angular.module('pigeonT', ['ngRoute'])
+		.config(['$routeProvider', function($routeProvider) {
+			$routeProvider
+				.when('/aboutme', {
+					templateUrl: 'templates/aboutme.html',
+					controller: 'aboutmeController' 
+				})
+				.when('/interests', {
+					templateUrl: 'templates/interests.html',
+					controller: 'interestsController' 
+				})
+				.when('/reading', {
+					templateUrl: 'templates/reading.html',
+					controller: 'readingController' 
+				})
+				.when('/contacts', {
+					templateUrl: 'templates/contacts.html',
+					controller: 'contactsController' 
+				});
+		}])
+		.controller('containerController', function($scope) {
+			
+		})
+		.controller('aboutmeController', function($scope) {;
 
-	app
-		.controller('containerController', ['$scope', function($scope) {
+		})
+		.controller('interestsController', function($scope) {
 			
-		}])
-		.controller('aboutmeController', ['$scope', function($scope) {
-			
-		}])
-		.controller('interestsController', ['$scope', function($scope) {
-			
-		}])
-		.controller('readingController', ['$scope', function($scope) {
-			
-		}])
-		.controller('contactsController', ['$scope', function($scope) {
-			
-		}]);
+		})
+		.controller('readingController', function($scope) {;
 
-	document.addEventListener('DOMContentLoaded', function() {
-		//bootstrap angular
-	    angular.element(document).ready(function() {
-	      angular.bootstrap(document, ['pigeonT']);
-	    });
-	}, false);
-	
+		})
+		.controller('contactsController', function($scope) {;
+
+		});
+
+    angular.element(document).ready(function() {
+      angular.bootstrap(document, ['pigeonT']);
+    });
 });
